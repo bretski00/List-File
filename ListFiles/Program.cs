736311bbd0b5c8e164e0c0d5b5265212765bbe7b
Bret Huber <bretski00@gmail.com>
2014-10-13 12:39:38 -0500

    initial commit

---
+++ ListFiles/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace ListFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryToRead;
+            string fileName;
+            if (args == null || args.Length < 2)
+            {
+                Console.Write("Directory to Read > ");
+                directoryToRead = Console.ReadLine();
+                Console.Write("File Name to Write > ");
+                fileName = Console.ReadLine();
+            }
+            else
+            {
+                directoryToRead = args[0];
+                fileName = args[1];
+            }
+
+            if (directoryToRead == null || fileName == null || directoryToRead.Length == 0 || fileName.Length == 0) return;
+
+            using (var fileOutput = new System.IO.StreamWriter(fileName))
+            {
+                foreach (var file in System.IO.Directory.GetFiles(directoryToRead))
+                {
+                    fileOutput.WriteLine(file.Split('\\').LastOrDefault());
+                }
+            }
+        }
+    }
+}
